c241098bc462204def36aa3f1288068d53c7190e
dxn-scrlt <dx.scarlet.xn@gmail.com>
2026-07-27 20:30:26 -0400

    refactor: add dom references

---
+++ js/ui.js
@@ -0,0 +1,10 @@
+const locationName = document.getElementById('location-name');
+
+const weatherIcon = document.getElementById('weather-icon');
+
+const temperature = document.getElementById('temperature');
+const condition = document.getElementById('condition');
+const currentTime = document.getElementById('current-time');
+
+const statusMessage = document.getElementById('status-message');
+const locationButton = document.getElementById('location-button');
