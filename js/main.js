db6b3e2e0fce5faed8767ec8253185b2abc1f37b
dxn-scrlt <dx.scarlet.xn@gmail.com>
2026-07-29 14:21:29 -0400

    feat: add async geolocation initialization

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
-function init() {}
+import { WEATHER_CODES } from './weather-codes.js';
+import { getLocationDetails } from './location-api.js';
+import { getWeather } from './weather-api.js';
+import { updateTime, updateLocation, updateWeather, updateWeatherTheme } from './ui.js';
+
+async function init() {
+    updateTime(new Date());
+
+    const coordinates = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            position => resolve(position.coords),
+            reject
+        );
+    });
+    const { latitude, longitude } = coordinates;
+}
 
 init();
