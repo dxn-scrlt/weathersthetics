0571a72b62b1d883810371f7fdfa2d9299834b4e
dxn-scrlt <dx.scarlet.xn@gmail.com>
2026-07-30 00:44:26 -0400

    feat: implement app initialization

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 import { WEATHER_CODES } from './weather-codes.js';
 import { getLocationDetails } from './location-api.js';
 import { getWeather } from './weather-api.js';
-import { updateTime, updateLocation, updateWeather, updateWeatherTheme } from './ui.js';
+import { updateTime, updateLocation, updateWeather, updateWeatherTheme, setPlaceholder } from './ui.js';
 
 async function init() {
+    const placeholder = {
+        place: 'jersey city',
+        weather: {
+            temperature: 72,
+            condition: WEATHER_CODES[0],
+            assets: {
+                icon: '',
+                background: '',
+                favicon: ''
+            }
+        }
+    };
+    setPlaceholder(placeholder);
     updateTime(new Date());
 
     const coordinates = await new Promise((resolve, reject) => {
@@ -13,6 +26,33 @@ async function init() {
         );
     });
     const { latitude, longitude } = coordinates;
+
+    const locationData = await getLocationDetails(latitude, longitude);
+    const weatherData = await getWeather(latitude, longitude);
+
+    const address = locationData.address;
+    const place =
+        address.city ??
+        address.town ??
+        address.municipality ??
+        address.village ??
+        address.hamlet;
+    updateLocation(place);
+
+    const currentWeather = weatherData.current;
+    const weather = {
+        temperature: currentWeather.temperature_2m,
+        condition: WEATHER_CODES[currentWeather.weather_code],
+        assets: {
+            icon: '',
+            background: '',
+            favicon: ''
+        }
+    };
+    updateWeather(weather);
+    updateWeatherTheme(weather);
+
+    updateTime(new Date());
 }
 
 init();
